65d02c048dccd5a35035ed0f064e16f5fd2e98f1
Gadiyatov Anton <gadiyatov.an@gmail.com>
2021-08-23 20:41:02 +0500

    feat: add morde decoder logic

--- src/index.js
+++ src/index.js
@@ -38,7 +38,45 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    {
+
+        let messageArr = [];
+        let letterArr = [];
+        let morseArr = [];
+        let newMessage = '';
+    
+        for (let i = 0; i < expr.length; i += 10) {
+            messageArr.push(expr.slice(i, i + 10));
+        }
+    
+        messageArr.forEach(letter => {
+            if (letter.indexOf(1) != -1) {
+                letterArr.push(letter.slice(letter.indexOf(1)));
+            } else letterArr.push(' ');
+        })
+    
+        letterArr.forEach(letter => {
+            let singleLetter = '';
+            if (letter.length == 1) return morseArr.push(' ');
+            for (let i = 0; i < letter.length; i += 2) {
+                if (letter[i + 1] == 0) {
+                    singleLetter += '.'
+                } else if (letter[i + 1] == 1) {
+                    singleLetter += '-'
+                }
+    
+            }
+            morseArr.push(singleLetter);
+        })
+    
+        morseArr.forEach(letter => {
+            if (letter === " ") newMessage += ' ';
+            for (const [key, value] of Object.entries(MORSE_TABLE)) {
+                if (letter == key) { newMessage += value }
+            }
+        })
+        return newMessage;
+    }
 }
 
 module.exports = {
